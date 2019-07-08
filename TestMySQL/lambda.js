a121bdb747f48b5a78400b9be8ab1298a1e4cbe6
sigmavcs <jchamath+sigma@gmail.com>
2019-07-08 11:39:51 +0530

    my commit

--- TestMySQL/lambda.js
+++ TestMySQL/lambda.js
@@ -1,21 +1,4 @@
-const mysql = require('mysql');
-
-exports.handler = function (event, context, callback) {
-
-    var connection = mysql.createConnection({
-        host: 'mysql-rfam-public.ebi.ac.uk',
-        user: 'rfamro',
-        port: 4497,
-        database: 'Rfam'
-    });
-
-    connection.connect();
-
-    connection.query('SELECT * FROM family LIMIT 5', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results);
-        callback(null, { results });
-    });
-
-    connection.end();
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
